10bd1dcdd7f826236bab422d020d8adb8ddbb7e0
pedrosh101 <pedro.sh101@gmail.com>
2022-10-11 14:37:23 -0300

    111022 commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,19 +7,10 @@ function HeroSection() {
   return (
     <>
       <Navbar />
-      <div className="hero-container">
-        <video src="videos/video-1.mp4" autoPlay loop muted />
+      <div className="heroContainer">
+        <video src="videos/video.mp4" autoPlay loop muted />
         <h1>Beauty Spa</h1>
         <p>Venha conhecer nosso espaço</p>
-        <div className="hero-btns">
-          {/* <Button
-            className="btns"
-            buttonStyle="btn--primary"
-            buttonSize="btn--large"
-          >
-            Dependências
-          </Button> */}
-        </div>
       </div>
       <Footer />
     </>
